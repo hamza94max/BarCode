1ef11ef0e8c76096b4d5ff14078336f3c9308bdb
Hamza Gamal <54688005+hamza94max@users.noreply.github.com>
2022-01-22 13:10:45 +0200

    Fix issue : library from home fragment

--- app/src/main/java/com/hamza/barcode/activites/MainActivity.kt
+++ app/src/main/java/com/hamza/barcode/activites/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.hamza.barcode.activites
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.hamza.barcode.Fragments.ExpiredItemsFragment
 import com.hamza.barcode.Fragments.HomeFragment
-
 import com.hamza.barcode.R
 import kotlinx.android.synthetic.main.activity_main.*
 
-
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +36,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun setCurrentFragment(fragment: Fragment) =
         supportFragmentManager.beginTransaction().apply {
-            replace(R.id.flFragment, fragment)
+            replace(R.id.framelayout, fragment)
             commit()
+
+            bottomNavigationView.visibility = View.VISIBLE
         }
 }
